466ca2dc5d86452f8bd4cc127287bd2d761c7a98
Dragan <drad@Dragans-MacBook-Pro.local>
2026-02-27 15:01:00 +0100

    Add batch lookup and fuzzy search

---
+++ games/frontend/images/Akademie_files/451f2af816302b07.js
@@ -0,0 +1,5 @@
+;!function(){try { var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof global?global:"undefined"!=typeof window?window:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&((e._debugIds|| (e._debugIds={}))[n]="fa718bfd-ffcf-12ad-6749-cd62a2a78e4a")}catch(e){}}();
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,527740,e=>{"use strict";var i=e.i(188547),o=e.i(898483);e.s(["default",0,({children:e})=>(0,i.jsx)(o.ChangelogProvider,{children:e})])}]);
+
+//# debugId=fa718bfd-ffcf-12ad-6749-cd62a2a78e4a
+//# sourceMappingURL=31f4551f470d97fb.js.map
